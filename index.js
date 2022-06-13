c9173ea7aaff59e305f3cd865752e68499efd45b
Jon Sparks <jon.sparks@sotic.net>
2022-06-13 16:41:22 +0100

    attempt to fix

--- index.js
+++ index.js
@@ -5,43 +5,38 @@ const app = express()
 const port = 8080
 
 async function getData() {
+  
+  const url = `http://carfromjapan.com/cheap-used-toyota-for-sale?keywords=gx81&sort=-createdAt&limit=999&minYear=1989&maxYear=1993`
 
-  return {
-    title: 'Hello World',
-    stuff: [1,2,3,4,5]
-  }
+  const browser = await puppeteer.launch({})
+  const page = await browser.newPage()
 
-  // const url = `https://carfromjapan.com/cheap-used-toyota-for-sale?keywords=gx81&sort=-createdAt&limit=999&minYear=1989&maxYear=1993`
+  await page.goto(url)
 
-  // const browser = await puppeteer.launch({})
-  // const page = await browser.newPage()
+  const carRows = await page.$$(`.car-row`)
 
-  // await page.goto(url)
+  const cars = await Promise.all(carRows.map(async (row) => {
+    const id = await row.$eval(`td:nth-of-type(2) .text-color`, child => child.textContent)
+    const model = await row.$eval(`td:nth-of-type(2) h2`, child => child.textContent)
+    const year = await row.$eval(`td:nth-of-type(3)`, child => child.textContent.slice(0, 4))
+    const mileage = await row.$eval(`td:nth-of-type(4)`, child => child.textContent.slice(0, -2))
+    const grade = await row.$eval(`td:nth-of-type(6)`, child => child.textContent.slice(6))
+    const price = await row.$eval(`td:nth-of-type(7) strong`, child => child.textContent.slice(4))
+    const url = await row.$eval(`td:nth-of-type(1)`, child => child.querySelector(`a`).href)
+    const dateScraped = new Date().toISOString()
+    return await {
+      id,
+      model,
+      year,
+      mileage,
+      grade,
+      price,
+      url,
+      dateScraped
+    }
+  }))
 
-  // const carRows = await page.$$(`.car-row`)
-
-  // const cars = await Promise.all(carRows.map(async (row) => {
-  //   const id = await row.$eval(`td:nth-of-type(2) .text-color`, child => child.textContent)
-  //   const model = await row.$eval(`td:nth-of-type(2) h2`, child => child.textContent)
-  //   const year = await row.$eval(`td:nth-of-type(3)`, child => child.textContent.slice(0, 4))
-  //   const mileage = await row.$eval(`td:nth-of-type(4)`, child => child.textContent.slice(0, -2))
-  //   const grade = await row.$eval(`td:nth-of-type(6)`, child => child.textContent.slice(6))
-  //   const price = await row.$eval(`td:nth-of-type(7) strong`, child => child.textContent.slice(4))
-  //   const url = await row.$eval(`td:nth-of-type(1)`, child => child.querySelector(`a`).href)
-  //   const dateScraped = new Date().toISOString()
-  //   return await {
-  //     id,
-  //     model,
-  //     year,
-  //     mileage,
-  //     grade,
-  //     price,
-  //     url,
-  //     dateScraped
-  //   }
-  // }))
-
-  // return cars
+  return cars
   
 }
 
